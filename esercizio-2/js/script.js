3e5007eddddd74f3c67fbced75c9a5f49e5d4854
Giorgio <shadigiorgioislam@gmail.com>
2021-01-20 10:18:41 +0100

    last fix

--- esercizio-2/js/script.js
+++ esercizio-2/js/script.js
@@ -1,6 +1,6 @@
  // Utilizzando const e/o let e template literal. Creare due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l’altro di verde. Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
 
-let arrayNumeri = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const arrayNumeri = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 for (let i = 0; i < arrayNumeri.length; i++) {
   if (arrayNumeri[i] % 2) {
